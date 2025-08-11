1f3528ea844b95f708491d2a62e0eb7c511334fa
Pranav Mishra <mishrapranav0104@gmail.com>
2025-08-12 04:44:11 +0530

    Updated mongodb uri

--- todo-backend/db/db.js
+++ todo-backend/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-const connectToDb = () => {
-    mongoose.connect(process.env.DB_CONNECT)
+const connectToDb =  () => {
+     mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('MongoDB connection error:', err));
 };
--- todo-backend/index.js
+++ todo-backend/index.js
@@ -8,7 +8,7 @@ const connectToDb = require('./db/db');
 dotenv.config();
 connectToDb();
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 
 app.use(cors());
 app.use(express.json());
